d98361186827f04411ea3bc0b94f2502439d6463
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-28 14:37:31 -0700

    add create route and controller method

--- app/controllers/api/v1/post_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::PostController < ApplicationController
-
-    def index
-        posts = Post.all
-        render json: posts
-    end
-
-end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::PostsController < ApplicationController
         render json: posts
     end
 
+    def create 
+        post = Post.new(post_params)
+        post.user = User.last
+        if post.save
+            render json: post
+        else
+            render json: {error: 'Error creating post'}
+        end
+    end
+
+    private 
+
+    def post_params
+        params.require(:post).permit(:title, :description, :image_url, :url)
+    end
+
 end
