b2bec8ad8cc20e44ecd9a3cf664dc2fe800581df
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-31 14:06:43 -0700

    controllers for user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+
+    has_secure_password
 
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+if Rails.env === 'production' 
+    Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'http://localhost:3001/'
+  else
+    Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
+end
+
