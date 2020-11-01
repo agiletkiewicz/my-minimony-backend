580efe682083c54c5ecece74d6eae38d28644b1d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-31 18:28:20 -0700

    comment out helper mothods

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
     
     def create
-        @user = User.find_by(email: session_params[:email])
+        @user = User.find_by(username: session_params[:username])
       
         if @user && @user.authenticate(session_params[:password])
           login!
