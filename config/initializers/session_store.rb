55e968ff9ceeaf08da51213333723d0387689696
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-31 20:10:51 -0700

    user successfully logs in

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'http://localhost:3001/'
+    Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'http://localhost:3001'
   else
     Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
 end
