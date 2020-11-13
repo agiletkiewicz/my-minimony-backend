33451163ebbcc4dae88131931a8f37dee8725aea
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-13 15:32:08 -0800

    add dependent destroy to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    has_many :boards_post
+    has_many :boards_post, dependent: :destroy
     has_many :boards, through: :boards_post
 
     validates :title, presence: true 
