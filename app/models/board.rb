d83e410866e2d5cdf1f7c5349a8a0bcb03b4176b
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-12 16:28:22 -0800

    fix board image bug

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,12 @@ class Board < ApplicationRecord
 
   validates :title, presence: true
 
+  def image 
+    if self.posts.first
+      self.posts.first.image_url
+    else
+      "https://images.unsplash.com/photo-1551554842-b37d2131aa68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    end
+  end
+
 end
--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,6 +1,6 @@
 class BoardSerializer
     include JSONAPI::Serializer
-    attributes :title, :id
+    attributes :title, :id, :image
     set_key_transform :camel_lower
 
     has_many :boards_post, serializer: BoardsPostSerializer
