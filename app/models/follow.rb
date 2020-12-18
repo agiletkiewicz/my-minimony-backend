ef206756eda184712758f9be8e4f0fda6afb8293
agiletkiewicz <a.letkiewicz@gmail.com>
2020-12-18 00:23:58 -0800

    render title and image of boards followed

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,12 @@
 class Follow < ApplicationRecord
   belongs_to :user
   belongs_to :board
+
+  def image
+    self.board.image
+end
+
+def title
+    self.board.title
+end
 end
--- app/serializers/follow_serializer.rb
+++ app/serializers/follow_serializer.rb
@@ -2,7 +2,8 @@ class FollowSerializer
     include JSONAPI::Serializer
     set_key_transform :camel_lower
     
-    attributes :id, :user_id, :board_id
-    
+    attributes :id, :user_id, :board_id, :title, :image
+
+
     
 end
