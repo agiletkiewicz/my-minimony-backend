d4a25fa9081453469f36f2cb92bb707315a81e88
agiletkiewicz <a.letkiewicz@gmail.com>
2020-12-21 23:48:03 -0800

    install active storage and add attachement url to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
     
     validates :title, :description, :image_url, presence: true 
 
+    has_one_attached :attachment
+
+    def attachment_url
+        Rails.application.routes.url_helpers.rails_representation_url(
+          attachment.variant(resize_to_limit: [200, 200]).processed, only_path: true
+        )
+    end
+
 end
