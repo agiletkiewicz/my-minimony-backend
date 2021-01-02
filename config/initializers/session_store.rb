9e3d28309ea92095ef74c25e3de76aad4ed3f082
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-02 11:01:05 -0800

    test config ssl to true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,12 @@
 # if Rails.env === 'production' 
-  # Rails.application.config.session_store :cookie_store, key: '_my-minimony', secure: Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_my-minimony', secure: Rails.env.production?
 # else
 #   Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
 # end
 
 
-if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'https://www.myminimonyapp.com/'
-else
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
-end
+# if Rails.env == 'production' 
+#   Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'https://www.myminimonyapp.com/'
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
+# end
