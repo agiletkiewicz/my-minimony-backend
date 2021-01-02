5ad082cdc6fccf140b97bcfe40b0aba18321232e
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-02 13:14:29 -0800

    test same site: none in session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,16 @@
 # end
 
 
-if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'myminimonyapp.com'
-else
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
-end
+# if Rails.env == 'production' 
+#   Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'myminimonyapp.com'
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
+# end
+
+Rails.application.config.session_store :cookie_store, {
+  :key => '_my-minimony',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
