16234ffa1ba4f0b703e09b146ca2e7410309ab45
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-01 16:11:09 -0800

    validate urls before saving to database

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,8 @@
+require 'open-uri'
+
 class Api::V1::PostsController < ApplicationController
 
     def index
-        # byebug
         if params[:user_id]
             posts = User.find_by(id: params[:user_id]).posts
             render json: PostSerializer.new(posts)
@@ -12,7 +13,25 @@ class Api::V1::PostsController < ApplicationController
     end
 
     def create 
+
+        if invalid_image_url? 
+            render json: {
+                status: 422, 
+                error: "Invalid image URL"
+            }
+            return
+        end
+
+        if invalid_url? 
+            render json: {
+                status: 422, 
+                error: "Invalid URL"
+            }
+            return
+        end
+
         post = current_user.posts.build(post_params)
+
         if post.save
             render json: PostSerializer.new(post)
         else
@@ -38,6 +57,34 @@ class Api::V1::PostsController < ApplicationController
 
     def post_params
         params.permit(:title, :description, :image_url, :url, :image)
+    end
+
+    def invalid_image_url?
+        if params[:image_url] != ""
+            begin 
+                open(params[:image_url]).status != ["200", "OK"]
+            rescue Errno::ENOENT
+                true
+            else 
+                false
+            end
+        else 
+            false 
+        end
+    end
+
+    def invalid_url?
+        if params[:url] != ""
+            begin 
+                open(params[:url]).status != ["200", "OK"]
+            rescue Errno::ENOENT
+                true
+            else 
+                false
+            end
+        else
+            false
+        end
     end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
+
 class Post < ApplicationRecord
     include Rails.application.routes.url_helpers
 
     belongs_to :user
     has_many :boards_post, dependent: :destroy
     has_many :boards, through: :boards_post
-    
-    validates :title, :description, presence: true 
 
     has_one_attached :image
+    
+    validates :title, :description, presence: true
 
     def get_image_url
         if self.image_url != ""
@@ -15,6 +16,6 @@ class Post < ApplicationRecord
         else
             url_for(self.image)
         end
-      end
+    end
 
 end
