9b77e7f1e7e52cc4dffc5a088ae9c15dbe4773b3
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-01 22:52:14 -0800

    test change session store file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'https://www.myminimonyapp.com/'
-else
-  Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
-end
+# if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_my-minimony', secure: Rails.env.production?
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
+# end
