7bfc891a125fedaa4994fbf867deabcf3f169302
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-01 20:56:35 -0800

    fix cors origin bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,21 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://affectionate-mclean-f2b3fb.netlify.app, http://www.myminimonyapp.com'
+    origins 'https://affectionate-mclean-f2b3fb.netlify.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+
+  allow do
+    origins 'http://www.myminimonyapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
 end
