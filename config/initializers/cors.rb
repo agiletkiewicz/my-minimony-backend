8d151a3650f1ed4e935f48243ba4657a418f8f75
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-01 20:30:29 -0800

    change origin URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://affectionate-mclean-f2b3fb.netlify.app'
 
     resource '*',
       headers: :any,
