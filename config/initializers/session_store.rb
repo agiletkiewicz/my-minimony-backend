ec16ed85bd9ad538d643a9e9ac6b039d174bd3a3
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-01 21:32:47 -0800

    test session store url change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env === 'production' 
     Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'https://www.myminimonyapp.com'
   else
-    Rails.application.config.session_store :cookie_store, key: '_my-minimony' 
+    Rails.application.config.session_store :cookie_store, key: '_my-minimony', domain: 'https://www.myminimonyapp.com'
 end
 
