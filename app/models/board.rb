5ac24b70fa5957dc603e7e91ad9ca085bca9b8b2
agiletkiewicz <a.letkiewicz@gmail.com>
2021-02-03 19:59:35 -0700

    add 4 images to board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -15,4 +15,36 @@ class Board < ApplicationRecord
     end
   end
 
+  def image_one 
+    if self.posts.first
+      self.posts.first.get_image_url 
+    else
+      "https://images.unsplash.com/photo-1551554842-b37d2131aa68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    end
+  end
+
+  def image_two 
+    if self.posts[1]
+      self.posts[1].get_image_url 
+    else
+      "https://images.unsplash.com/photo-1551554842-b37d2131aa68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    end
+  end
+
+  def image_three 
+    if self.posts[2]
+      self.posts[2].get_image_url 
+    else
+      "https://images.unsplash.com/photo-1551554842-b37d2131aa68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    end
+  end
+
+  def image_four 
+    if self.posts[3]
+      self.posts[3].get_image_url 
+    else
+      "https://images.unsplash.com/photo-1551554842-b37d2131aa68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    end
+  end
+
 end
--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,6 +1,6 @@
 class BoardSerializer
     include JSONAPI::Serializer
-    attributes :title, :id, :image
+    attributes :title, :id, :image, :image_one, :image_two, :image_three, :image_four
     set_key_transform :camel_lower
 
     has_many :boards_post, serializer: BoardsPostSerializer
