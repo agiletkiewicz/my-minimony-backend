d96c9669cee44e60c838471aeeba5cbe90c9a00a
agiletkiewicz <a.letkiewicz@gmail.com>
2021-02-03 20:02:21 -0700

    transform board attributes when sending JSON

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,7 +1,8 @@
 class BoardSerializer
     include JSONAPI::Serializer
-    attributes :title, :id, :image, :image_one, :image_two, :image_three, :image_four
     set_key_transform :camel_lower
+
+    attributes :title, :id, :image, :image_one, :image_two, :image_three, :image_four
 
     has_many :boards_post, serializer: BoardsPostSerializer
     
